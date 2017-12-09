f72c1bae6ee039b0b45183e9fddf9d24e3b23eb3
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-12-09 12:05:37 +0530

    adding new code

---
+++ basics.go
@@ -0,0 +1,31 @@
+package main
+
+
+import "fmt"
+
+
+func main(){
+
+
+  var number int = 7
+
+  var floating float64 = 1.723233
+
+  value := 1
+  //once declared as a number cant give string value later on in the program
+
+  fmt.Println(number,floating,value)
+  //automatically gives spacing
+
+
+  // const pi float64 = 3.14
+  //
+  // var(
+  //   one = 1
+  //   two = 2
+  // )
+
+  var name string = "Joseph"
+  fmt.Println(len(name))
+
+}
